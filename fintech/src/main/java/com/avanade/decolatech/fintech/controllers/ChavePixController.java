86c0944f7c0563809e54004cecd1e5607964f918
Weydson Fernandes <weydfernandes@gmail.com>
2025-01-28 23:09:30 -0300

    realizando login e redirecionando para pagina autenticada de chavesPix - teste

--- fintech/src/main/java/com/avanade/decolatech/fintech/controllers/ChavePixController.java
+++ fintech/src/main/java/com/avanade/decolatech/fintech/controllers/ChavePixController.java
@@ -6,18 +6,14 @@ import com.avanade.decolatech.fintech.models.dtos.responses.ChavePixResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.avanade.decolatech.fintech.models.entities.ChavePix;
 import com.avanade.decolatech.fintech.models.services.ChavePixService;
 
 @RestController
 @RequestMapping("/chavePix")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
 public class ChavePixController {
 	
 	@Autowired
